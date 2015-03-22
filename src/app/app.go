ab6cae4443d7d04e8df462d46059bebd80e7ee37
Nathan Moos <moosingin3space@gmail.com>
2015-03-21 22:46:47 -0400

    up to parity with Clojure version

--- src/app/app.go
+++ src/app/app.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	httpc "httpclient"
 	"mbus"
 	"net/http"
@@ -20,11 +19,7 @@ func init() {
 }
 
 func closestStop(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(500)
-	fmt.Fprint(w, "Not implemented")
-}
-
-func closestStops(w http.ResponseWriter, r *http.Request) {
+	// extract latitude and longitude from URL
 	lat_string := r.URL.Query().Get("lat")
 	lon_string := r.URL.Query().Get("lon")
 	latitude, err := strconv.ParseFloat(lat_string, 64)
@@ -37,6 +32,8 @@ func closestStops(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+
+	// create HTTP client and fetch stop list
 	c := httpc.Client(r)
 	stops, err := mbus.StopList(c)
 	if err != nil {
@@ -47,6 +44,46 @@ func closestStops(w http.ResponseWriter, r *http.Request) {
 	// now sort by distance
 	sort.Sort(util.ByStopDistance(stops, latitude, longitude))
 
+	// closest stop
+	stop := stops[0]
+
+	// write this stop
+	blob, err := json.Marshal(stop)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(blob)
+}
+
+func closestStops(w http.ResponseWriter, r *http.Request) {
+	// extract latitude and longitude from URL
+	lat_string := r.URL.Query().Get("lat")
+	lon_string := r.URL.Query().Get("lon")
+	latitude, err := strconv.ParseFloat(lat_string, 64)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	longitude, err := strconv.ParseFloat(lon_string, 64)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// create HTTP client and fetch stop list
+	c := httpc.Client(r)
+	stops, err := mbus.StopList(c)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// now sort by distance
+	sort.Sort(util.ByStopDistance(stops, latitude, longitude))
+
+	// Write final set of closest stops
 	blob, err := json.Marshal(stops[:NUM_CLOSEST_STOPS])
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -57,6 +94,28 @@ func closestStops(w http.ResponseWriter, r *http.Request) {
 }
 
 func nextBus(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(500)
-	fmt.Fprint(w, "Not implemented")
+	// get stop from URL
+	stopId_string := r.URL.Query().Get("stop")
+	stopId, err := strconv.Atoi(stopId_string)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// create HTTP client and fetch next bus
+	c := httpc.Client(r)
+	bus, err := mbus.NextBusAtStop(c, stopId)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// now write that bus
+	blob, err := json.Marshal(bus)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(blob)
 }
--- src/mbus/api.go
+++ src/mbus/api.go
@@ -3,9 +3,16 @@ package mbus
 import (
 	"data"
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 )
+
+type Route struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
 
 const API_ENDPOINT = "http://mbus.doublemap.com/map/v2"
 
@@ -21,5 +28,57 @@ func StopList(cl *http.Client) (stops []data.Stop, err error) {
 		return
 	}
 	err = json.Unmarshal(body, &stops)
+	return
+}
+
+func NextBusAtStop(cl *http.Client, stopId int) (bus data.Bus, err error) {
+	etasUrl := API_ENDPOINT + "/eta?stop=" + strconv.Itoa(stopId)
+	routesUrl := API_ENDPOINT + "/routes"
+	etaResp, err := cl.Get(etasUrl)
+	if err != nil {
+		return
+	}
+	routeResp, err := cl.Get(routesUrl)
+	defer etaResp.Body.Close()
+	defer routeResp.Body.Close()
+	etaBody, err := ioutil.ReadAll(etaResp.Body)
+	routeBody, err := ioutil.ReadAll(routeResp.Body)
+	if err != nil {
+		return
+	}
+	// read etas
+	var etas map[string]interface{}
+	err = json.Unmarshal(etaBody, &etas)
+	if err != nil {
+		return
+	}
+	innerEtas := etas["etas"].(map[string]interface{})[strconv.Itoa(stopId)].(map[string]interface{})["etas"].([]interface{})
+	if len(innerEtas) > 0 {
+		first_eta := innerEtas[0].(map[string]interface{})
+		routeId := int(first_eta["route"].(float64))
+
+		// read routes
+		var routes []Route
+		err = json.Unmarshal(routeBody, &routes)
+		if err != nil {
+			return
+		}
+		for _, r := range routes {
+			if r.Id == routeId {
+				time, ok := first_eta["avg"].(float64)
+				if ok {
+					// our route
+					bus = data.Bus{
+						Text: r.Name,
+						Time: int(time),
+					}
+					return
+				} else {
+					err = errors.New("invalid type")
+					return
+				}
+			}
+		}
+	}
 	return
 }
